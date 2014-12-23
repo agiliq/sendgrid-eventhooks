2cbafb71b212090ba12219854489207e285b02e5
Akshar Raaj <akshar@agiliq.com>
2014-12-23 12:43:54 +0530

    Add an example_app to capture the signal raised by sendgrid_events.

---
+++ example/example_app/models.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.dispatch import receiver
+
+from sendgrid_events.views import sendgrid_email_received
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+@receiver(sendgrid_email_received)
+def sendgrid_email_received_callback(sender, **kwargs):
+    data = kwargs['data']
+    logging.info(data['Sender'])
+    logging.info(data['Subject'])
+    logging.info(data['Body'])
