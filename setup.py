acbfc7a466509cdf6c2dc2a2829c7acce31d0581
Akshar Raaj <akshar@agiliq.com>
2014-12-23 15:23:14 +0530

    Added setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = 'sendgrid_eventhooks',
+  packages = ['sendgrid_events'], # this must be the same as the name above
+  version = '0.1.1',
+  description = 'Django app to receive incoming email notification events from sendgrid',
+  long_description = read("README.md"),
+  author = 'Agiliq Info Solutions',
+  author_email = 'hello@agiliq.com',
+  url = 'https://github.com/agiliq/sendgrid-eventhooks', # use the URL to the github repo
+  classifiers = [],
+)
